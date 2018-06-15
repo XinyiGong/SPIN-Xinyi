9a3456c83ea6dac80b8deb27267d8efd0075bce7
Xinyi Gong <xgonggt@gmail.com>
2018-06-15 16:06:47 -0400

    Revert "Major Modifications"
    This reverts commit 260e1fc242cba1d6b6ed9dd7c8742bf960b78b44.

--- SPIN_vJun18/Analyze.m
+++
@@ -1,5 +0,0 @@
-function [SearchResults, npoints, HistSearchResults, mflag] = Analyze(TestData, seg_sizes, filt, Plastic, BEuler, bins, wr, wr2, limx,limzerox,scatterplot, histplot,sliderplot)
-
-FitResults = SingleSearchAllSegments(seg_sizes, TestData);
-[SearchResults, npoints, HistSearchResults, mflag] = SearchExplorer(TestData, FitResults, filt, Plastic, BEuler, bins, wr, wr2, limx,limzerox,scatterplot, histplot,sliderplot);
-
